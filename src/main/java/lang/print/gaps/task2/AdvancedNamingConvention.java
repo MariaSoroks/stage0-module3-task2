a58f215bb99ffd96b8083af4db40a749b706b95b
MariaSoroks <sormash@gmail.com>
2022-12-21 10:31:56 +0600

    Naming conventions

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,16 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int adultAge = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void CallToFriend() {
+        //some code
+    }
+
+    void CallByNumber(int number) {
+        //some code
+    }
+}
